4557d6b39d73fa036562bc928de897812eff5d62
Robin Lau <Robi0279@stud.kea.dk>
2022-04-19 14:01:28 +0200

    ny

--- wordpress/mytheme/page-forside.php
+++ wordpress/mytheme/page-forside.php
@@ -27,23 +27,57 @@ get_header();
 		<div class="boks1">
 			<img src="Verdensmaal/Verdensmaal-ikon-01.png" alt="">
 		</div>
-		<div class="boks2"></div>
-		<div class="boks3"></div>
-		<div class="boks4"></div>
-		<div class="boks5"></div>
-		<div class="boks6"></div>
-		<div class="boks7"></div>
-		<div class="boks8"></div>
-		<div class="boks9"></div>
-		<div class="boks10"></div>
-		<div class="boks11"></div>
-		<div class="boks12"></div>
-		<div class="boks13"></div>
-		<div class="boks14"></div>
-		<div class="boks15"></div>
-		<div class="boks16"></div>
-		<div class="boks17"></div>
-		<div class="boks18"></div>
+		<div class="boks2">
+			<img src="Verdensmaal/Verdensmaal-ikon-02.png" alt="">
+		</div>
+		<div class="boks3">
+			<img src="Verdensmaal/Verdensmaal-ikon-03.png" alt="">
+		</div>
+		<div class="boks4">
+			<img src="Verdensmaal/Verdensmaal-ikon-04.png" alt="">
+		</div>
+		<div class="boks5">
+			<img src="Verdensmaal/Verdensmaal-ikon-05.png" alt="">
+		</div>
+		<div class="boks6">
+			<img src="Verdensmaal/Verdensmaal-ikon-06.png" alt="">
+		</div>
+		<div class="boks7">
+			<img src="Verdensmaal/Verdensmaal-ikon-07.png" alt="">
+		</div>
+		<div class="boks8">
+			<img src="Verdensmaal/Verdensmaal-ikon-08.png" alt="">
+		</div>
+		<div class="boks9">
+			<img src="Verdensmaal/Verdensmaal-ikon-09.png" alt="">
+		</div>
+		<div class="boks10">
+			<img src="Verdensmaal/Verdensmaal-ikon-10.png" alt="">
+		</div>
+		<div class="boks11">
+			<img src="Verdensmaal/Verdensmaal-ikon-11.png" alt="">
+		</div>
+		<div class="boks12">
+			<img src="Verdensmaal/Verdensmaal-ikon-12.png" alt="">
+		</div>
+		<div class="boks13">
+			<img src="Verdensmaal/Verdensmaal-ikon-13.png" alt="">
+		</div>
+		<div class="boks14">
+			<img src="Verdensmaal/Verdensmaal-ikon-14.png" alt="">
+		</div>
+		<div class="boks15">
+			<img src="Verdensmaal/Verdensmaal-ikon-15.png" alt="">
+		</div>
+		<div class="boks16">
+			<img src="Verdensmaal/Verdensmaal-ikon-16.png" alt="">
+		</div>
+		<div class="boks17">
+			<img src="Verdensmaal/Verdensmaal-ikon-17.png" alt="">
+		</div>
+		<div class="boks18">
+			<img src="Verdensmaal/FN-Verdensmaal-ikon-logo.png" alt="">
+		</div>
 	</div>
 </section>
 		</main><!-- #main -->
