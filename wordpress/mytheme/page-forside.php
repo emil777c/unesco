27007dff6119dfa15d7a4b39496b84eb95a8df75
emil777c <emil777c@stud.kea.dk>
2022-04-19 14:36:58 +0200

    commiit

--- wordpress/mytheme/page-forside.php
+++ wordpress/mytheme/page-forside.php
@@ -24,60 +24,24 @@ get_header();
 
 <section id="second_section">
 	<div class="section_wrapper">
-		<div class="boks1">
-			<img src="ikon01.png" alt="">
-		</div>
-		<div class="boks2">
-			<img src="ikon02.png" alt="">
-		</div>
-		<div class="boks3">
-			<img src="ikon03.png" alt="">
-		</div>
-		<div class="boks4">
-			<img src="ikon04.png" alt="">
-		</div>
-		<div class="boks5">
-			<img src="ikon05.png" alt="">
-		</div>
-		<div class="boks6">
-			<img src="ikon06.png" alt="">
-		</div>
-		<div class="boks7">
-			<img src="ikon07.png" alt="">
-		</div>
-		<div class="boks8">
-			<img src="ikon08.png" alt="">
-		</div>
-		<div class="boks9">
-			<img src="ikon09.png" alt="">
-		</div>
-		<div class="boks10">
-			<img src="ikon10.png" alt="">
-		</div>
-		<div class="boks11">
-			<img src="ikon11.png" alt="">
-		</div>
-		<div class="boks12">
-			<img src="ikon12.png" alt="">
-		</div>
-		<div class="boks13">
-			<img src="ikon13.png" alt="">
-		</div>
-		<div class="boks14">
-			<img src="ikon14.png" alt="">
-		</div>
-		<div class="boks15">
-			<img src="ikon15.png" alt="">
-		</div>
-		<div class="boks16">
-			<img src="ikon16.png" alt="">
-		</div>
-		<div class="boks17">
-			<img src="ikon17.png" alt="">
-		</div>
-		<div class="boks18">
-			<img src="logo.png" alt="">
-		</div>
+		<div class="boks1"></div>
+		<div class="boks2"></div>
+		<div class="boks3"></div>
+		<div class="boks4"></div>
+		<div class="boks5"></div>
+		<div class="boks6"></div>
+		<div class="boks7"></div>
+		<div class="boks8"></div>
+		<div class="boks9"></div>
+		<div class="boks10"></div>
+		<div class="boks11"></div>
+		<div class="boks12"></div>
+		<div class="boks13"></div>
+		<div class="boks14"></div>
+		<div class="boks15"></div>
+		<div class="boks16"></div>
+		<div class="boks17"></div>
+		<div class="boks18"></div>
 	</div>
 </section>
 		</main><!-- #main -->
