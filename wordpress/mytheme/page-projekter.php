6f423713d38f2273c84c59597ec5ec75bd26a2ab
emil777c <emil777c@stud.kea.dk>
2022-04-19 11:43:53 +0200

    first commit

---
+++ wordpress/mytheme/page-projekter.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since Twenty Nineteen 1.0
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+		<h1>Hej projekter
+        </h1>
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
