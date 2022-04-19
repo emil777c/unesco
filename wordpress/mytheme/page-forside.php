1d90c24206283abbdc63061af4fef7ea11c80252
Robin Lau <Robi0279@stud.kea.dk>
2022-04-19 13:34:40 +0200

    billede

--- wordpress/mytheme/page-forside.php
+++ wordpress/mytheme/page-forside.php
@@ -21,6 +21,31 @@ get_header();
 		<h3>Et verdensomspændende skolenetværk</h3>
 </div>
 </section>
+
+<section id="second_section">
+	<div class="section_wrapper">
+		<div class="boks1">
+			<img src="/Verdensmaal/Verdensmaal-ikon-01.png" alt="">
+		</div>
+		<div class="boks2"></div>
+		<div class="boks3"></div>
+		<div class="boks4"></div>
+		<div class="boks5"></div>
+		<div class="boks6"></div>
+		<div class="boks7"></div>
+		<div class="boks8"></div>
+		<div class="boks9"></div>
+		<div class="boks10"></div>
+		<div class="boks11"></div>
+		<div class="boks12"></div>
+		<div class="boks13"></div>
+		<div class="boks14"></div>
+		<div class="boks15"></div>
+		<div class="boks16"></div>
+		<div class="boks17"></div>
+		<div class="boks18"></div>
+	</div>
+</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
