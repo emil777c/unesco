ebe15b358c325f932f6d4bc908e5df0a848554bd
emil777c <emil777c@stud.kea.dk>
2022-04-20 10:44:20 +0200

    billeder

--- wordpress/mytheme/page-forside.php
+++ wordpress/mytheme/page-forside.php
@@ -24,24 +24,24 @@ get_header();
 
 <section id="second_section">
 	<div class="section_wrapper">
-		<div class="boks1"></div>
-		<div class="boks2"></div>
-		<div class="boks3"></div>
-		<div class="boks4"></div>
-		<div class="boks5"></div>
-		<div class="boks6"></div>
-		<div class="boks7"></div>
-		<div class="boks8"></div>
-		<div class="boks9"></div>
-		<div class="boks10"></div>
-		<div class="boks11"></div>
-		<div class="boks12"></div>
-		<div class="boks13"></div>
-		<div class="boks14"></div>
-		<div class="boks15"></div>
-		<div class="boks16"></div>
-		<div class="boks17"></div>
-		<div class="boks18"></div>
+		<div class="boks boks1"></div>
+		<div class="boks boks2"></div>
+		<div class="boks boks3"></div>
+		<div class="boks boks4"></div>
+		<div class="boks boks5"></div>
+		<div class="boks boks6"></div>
+		<div class="boks boks7"></div>
+		<div class="boks boks8"></div>
+		<div class="boks boks9"></div>
+		<div class="boks boks10"></div>
+		<div class="boks boks11"></div>
+		<div class="boks boks12"></div>
+		<div class="boks boks13"></div>
+		<div class="boks boks14"></div>
+		<div class="boks boks15"></div>
+		<div class="boks boks16"></div>
+		<div class="boks boks17"></div>
+		<div class="boks boks18"></div>
 	</div>
 </section>
 		</main><!-- #main -->
